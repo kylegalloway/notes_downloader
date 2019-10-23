c3b08ecbac830b0a881af85f267b85ee1de0d2a1
Kyle Galloway <kyle.galloway@cfdrc.com>
2019-10-23 15:49:21 -0500

    update enumerating lessons

--- download_notes.py
+++ download_notes.py
@@ -1,18 +1,49 @@
 from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
 
-for i in range(1, 39):
-    filename = "G01M01L{0:0=2d}_MC_StudentNotes_4thEd.pdf".format(i)
-    url = "https://webassets.zearn.org/lesson_pdfs/G01M01/{}".format(filename)
+module = 2
+lessons = [
+    1,
+    2,
+    3,
+    4,
+    6,
+    7,
+    8,
+    10,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    20,
+    22,
+    23,
+    25,
+    26,
+    27,
+    28,
+    29,
+]
+
+for lesson in lessons:
+    filename = "G01M{0:0=2d}L{1:0=2d}_MC_StudentNotes_4thEd.pdf".format(
+        module, lesson
+    )
+    url = "https://webassets.zearn.org/lesson_pdfs/G01M{0:0=2d}/{1}".format(
+        module, filename
+    )
     print("Opening url: {}".format(url))
     try:
         filedata = urlopen(url)
     except HTTPError as e:
-        print('The server couldn\'t fulfill the request.')
-        print('Error code: ', e.code)
+        print("The server couldn't fulfill the request.")
+        print("Error code: ", e.code)
     except URLError as e:
-        print('We failed to reach a server.')
-        print('Reason: ', e.reason)
+        print("We failed to reach a server.")
+        print("Reason: ", e.reason)
     else:
         datatowrite = filedata.read()
 
